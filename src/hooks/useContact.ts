024eb100b74a86944971a73116fdb64cab4d0745
mohammad6vakili <mohammad6vakili@gmail.com>
2023-08-26 18:27:24 +0330

    feat(add contactController): :sparkles: add contactController controller method

--- src/hooks/useContact.ts
+++ src/hooks/useContact.ts
@@ -1,5 +1,22 @@
+import { useFormik } from "formik";
+import { contactSchema } from "@/utils/schemas";
+
 const useContact = () => {
-  const exports = {};
+  // create provider form controller
+  const contactController = useFormik({
+    initialValues: {
+      contactName: "",
+      contactEmail: "",
+      contactSubject: "",
+      contactMessage: "",
+    },
+    validationSchema: contactSchema,
+    onSubmit: (values) => {
+      alert("Success.");
+    },
+  });
+
+  const exports = { contactController };
   return exports;
 };
 export default useContact;
